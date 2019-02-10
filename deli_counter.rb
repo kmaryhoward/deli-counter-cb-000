9e1df17faa34d8f95a59e99e428631b501ba0b21
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-10 19:17:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,19 @@ def line(line)
     if line.length == 0
       puts "The line is currently empty."
     else
-      line.each_with_index do |x, index|
-        a << "#{index+1}. #{x} "
+      line.each_with_index do |name, index|
+        a << "#{index+1}. #{name} "
     end
       puts "The line is currently:" + a.rstrip
    end
+end
+
+def take_a_number(line, name)
+  case
+    when line.length == 0
+      puts "Welcome #{name}. You are number 1 in line."
+    when line.length > 0
+      line << name
+      puts "Welcome #{name}. You are number #{line.length} in line."
+  end
 end
