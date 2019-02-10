556357621e5d220bdb33331f30c4aa0ca25767f0
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-10 18:55:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(line)
+  a = " "
+    if line.length == 0
+      puts "The line is currently empty."
+    else
+      line.each_with_index do |x, index|
+        a << "#{index+1}. #{x} "
+    end
+      puts "The line is currently:" + a.rstrip
+   end
+end
